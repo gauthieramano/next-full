c7cacafda65247ecf97a4c6f9c48573d76d76f83
Gauthier Amano <gauthier.net@gmail.com>
2025-07-15 17:18:37 +0200

    feat(app & providers & utils): ✨ add `ConvexClientProvider` + use it

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import { ConvexClientProvider } from "@/providers/convex-client";
 
 export const metadata: Metadata = {
   title: "PROJ9CT",
@@ -18,7 +19,9 @@ export default function RootLayout({ children }: Props) {
       lang="en"
       className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
     >
-      <body>{children}</body>
+      <body>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
