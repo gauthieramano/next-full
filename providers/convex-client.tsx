fa5b45e6a3ab06ef799f14d041b48c352517f245
Gauthier Amano <gauthier.net@gmail.com>
2025-07-15 20:20:53 +0200

    feat(global): ✨ add `ClerkProvider` & `clerkMiddleware` + use it
    env variables to add to `.env.local`:
    - `NEXT_PUBLIC_CLERK_FRONTEND_API_URL`
    - `NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY`
    - `CLERK_SECRET_KEY`

--- providers/convex-client.tsx
+++ providers/convex-client.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 import PUBLIC_ENV from "@/lib/public-env";
 
 const CONVEX = new ConvexReactClient(PUBLIC_ENV.CONVEX_URL);
@@ -8,5 +10,9 @@ const CONVEX = new ConvexReactClient(PUBLIC_ENV.CONVEX_URL);
 type Props = { children: React.ReactNode };
 
 export function ConvexClientProvider({ children }: Props) {
-  return <ConvexProvider client={CONVEX}>{children}</ConvexProvider>;
+  return (
+    <ConvexProviderWithClerk client={CONVEX} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
 }
