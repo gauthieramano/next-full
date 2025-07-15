980023d717a0ab61781e279c5f9ef258c61478cb
Gauthier Amano <gauthier.net@gmail.com>
2025-07-15 21:58:15 +0200

    feat(app & components): ✨ add `UserBadge` + use it

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { Button } from "@/components/ui/button";
+import UserBadge from "@/components/user-badge";
 
 export default function Home() {
+  const { user } = useUser();
+
+  const name = user?.fullName || undefined;
+
   return (
     <div className="font-sans grid items-center justify-items-center min-h-screen p-8 sm:p-20">
       <div className="flex flex-col gap-8 items-center">
         <Authenticated>
-          <UserButton />
+          <div className="flex gap-3 items-center">
+            <UserBadge name={name} />
+            <UserButton />
+          </div>
           <div>PRIVATE CONTENT</div>
         </Authenticated>
 
